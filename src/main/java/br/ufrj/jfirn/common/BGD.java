b79fb383a02ab18a4d5bd64b97ea91b4c84f8f8c
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-10-06 13:03:47 -0300

    Explicitly stating that literal numbers should be doubles.

--- src/main/java/br/ufrj/jfirn/common/BGD.java
+++ src/main/java/br/ufrj/jfirn/common/BGD.java
@@ -18,14 +18,14 @@ public class BGD {
 	private static final double B[] = {0.1337764, 0.6243247, 1.3425378, 2.2626645};
 
 	private static double f(double x, double y, double aprime, double bprime, double rho) {
-		return FastMath.exp(aprime * (2 * x - aprime) + bprime * (2 * y - bprime) + 2 * rho * (x - aprime) * (y - bprime));
+		return FastMath.exp(aprime * (2d * x - aprime) + bprime * (2d * y - bprime) + 2d * rho * (x - aprime) * (y - bprime));
 	}
 
 	// Numerical approximation to the bivariate normal distribution	
 	public static double cdf(double a, double b, double rho) {
 		if((a <= 0) && (b <= 0) && (rho <= 0)) {
-			final double aprime = a/FastMath.sqrt(2 * (1 - rho*rho));
-			final double bprime = b/FastMath.sqrt(2 * (1 - rho*rho));
+			final double aprime = a/FastMath.sqrt(2d * (1d - rho*rho));
+			final double bprime = b/FastMath.sqrt(2d * (1d - rho*rho));
 			double sum = 0;
 			for(int i = 0 ; i < A.length ; i++) {
 				for (int j = 0; j < A.length; j++) {
@@ -33,7 +33,7 @@ public class BGD {
 				}
 			}
 
-			sum *= FastMath.sqrt(1 - rho * rho) / FastMath.PI;
+			sum *= FastMath.sqrt(1d - rho * rho) / FastMath.PI;
 
 			return sum;
 		} else if(a * b * rho <= 0) {
@@ -45,10 +45,10 @@ public class BGD {
 				return normal.cumulativeProbability(a) + normal.cumulativeProbability(b) - 1 + cdf(-a, -b, rho);  
 			}
 		} else if(a * b * rho >= 0) {
-			final double denum = FastMath.sqrt(a * a - 2 * rho * a * b + b * b);
+			final double denum = FastMath.sqrt(a * a - 2d * rho * a * b + b * b);
 			final double rho1 = ((rho * a - b) * FastMath.signum(a)) / denum;
 			final double rho2 = ((rho * b - a) * FastMath.signum(b)) / denum;
-			final double delta = (1 - FastMath.signum(a) * FastMath.signum(b)) / 4d;
+			final double delta = (1d - FastMath.signum(a) * FastMath.signum(b)) / 4d;
 			return cdf(a, 0, rho1) + cdf(b, 0, rho2) - delta;
 		}
 
