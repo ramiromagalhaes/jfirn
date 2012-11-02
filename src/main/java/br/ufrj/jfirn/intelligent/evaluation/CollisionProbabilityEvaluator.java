5c4a27cd8da047d870ec5ec2e11704fe5664343c
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-02 11:23:47 -0200

    Fist implementation of the collision probability calculation.

--- src/main/java/br/ufrj/jfirn/intelligent/evaluation/CollisionProbabilityEvaluator.java
+++ src/main/java/br/ufrj/jfirn/intelligent/evaluation/CollisionProbabilityEvaluator.java
@@ -11,16 +11,17 @@ import br.ufrj.jfirn.intelligent.Thoughts;
 import br.ufrj.jfirn.intelligent.Trajectory;
 
 public class CollisionProbabilityEvaluator implements Evaluator {
-	private static final double RADIUS = 10;
 
 	@Override
 	public void evaluate(Thoughts thoughts, Instruction instruction, ChainOfEvaluations chain) {
 		final Map<Integer, MobileObstacleStatisticsLogger> knownObstacles =
 			thoughts.knownObstacles();
 
+		//The trajectory of the extremities of the IntelligentRobot, considering its movement direction
 		final Trajectory[] myTrajectory;
 		{
-			final Point points[] = makePoint(thoughts.myPosition(), thoughts.myDirection());
+			final Point points[] = RobotExtremePointsCalculator
+				.makePoint(thoughts.myPosition(), thoughts.myDirection());
 			myTrajectory = new Trajectory[] {
 				new Trajectory(thoughts.myDirection(), points[0]),
 				new Trajectory(thoughts.myDirection(), points[1]),
@@ -40,11 +41,23 @@ public class CollisionProbabilityEvaluator implements Evaluator {
 				myTrajectory[1].intersect(trajectories[1])
 			};
 
-			//TODO convert points to polar coordinates
-			intersections[0] = intersections[0];
-			intersections[1] = intersections[0];
-			intersections[2] = intersections[0];
-			intersections[3] = intersections[0];
+			final Point moPosition = stats.lastKnownPosition();
+
+			//Convert from XY coordinates to a polar coordinates around the mobile obstacle.
+			//Normalize the input, since BGD works only with 0 mean and 1 variance: (valor - mean) / sqrt(var)
+			for(int i = 0; i < intersections.length; i++) {
+				if (stats.directionVariance() != 0d) {
+					intersections[i] = new Point(
+						(moPosition.directionTo(intersections[i]) - stats.directionMean()) / FastMath.sqrt(stats.directionVariance()),
+						((moPosition.distanceTo(intersections[i]) / collision.time) - stats.speedMean()) / FastMath.sqrt(stats.speedVariance())
+					);
+				} else {
+					intersections[i] = new Point(
+						moPosition.directionTo(intersections[i]) - stats.directionMean(),
+						(moPosition.distanceTo(intersections[i]) / collision.time) - stats.speedMean()
+					);
+				}
+			}
 
 			collision.probability =
 				BGD.cdfOfHorizontalQuadrilaterals(
@@ -56,21 +69,6 @@ public class CollisionProbabilityEvaluator implements Evaluator {
 		}
 
 		chain.nextEvaluator(thoughts, instruction, chain);
-	}
-
-	private Point[] makePoint(Point myPosition, double myDirection) {
-		final double d1 = myDirection - FastMath.PI / 2d;
-		final Point[] points = new Point[] {
-			new Point(
-					myPosition.x() - FastMath.cos(d1) * RADIUS,
-					myPosition.y() - FastMath.sin(d1) * RADIUS
-			),
-			new Point(
-					myPosition.x() + FastMath.cos(d1) * RADIUS,
-					myPosition.y() + FastMath.sin(d1) * RADIUS
-			)
-		};
-		return points;
 	}
 
 }
