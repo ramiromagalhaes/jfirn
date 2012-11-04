8b19f0aaa037a8f2e3129694fefbf813ce12f727
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-04 12:50:22 -0200

    New class Interval.

---
+++ src/main/java/br/ufrj/jfirn/intelligent/evaluation/Interval.java
@@ -0,0 +1,19 @@
+package br.ufrj.jfirn.intelligent.evaluation;
+
+public class Interval extends org.apache.commons.math3.geometry.euclidean.oned.Interval {
+
+	public Interval(double lower, double upper) {
+		super(lower, upper);
+	}
+
+	/**
+	 * Verifies if x is inside this Interval.
+	 * 
+	 * @return true if x is a value between this Interval lower and upper
+	 *         bounds, false otherwise
+	 */
+	public boolean contains(double x) {
+		return this.getUpper() >= x && this.getLower() <= x;
+	}
+
+}
