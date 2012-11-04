fd96ae586fbd153f0bbd1721b2da162b8895d1e7
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-04 16:48:40 -0200

    Can't use the method to compare doubles I was using.

--- src/main/java/br/ufrj/jfirn/common/Point.java
+++ src/main/java/br/ufrj/jfirn/common/Point.java
@@ -119,10 +119,14 @@ public class Point {
 		@Override
 		public int compare(Point p1, Point p2) {
 			if (p1.y != p2.y) {
-				return (int)(p1.y - p2.y);
-			} else {
-				return (int)(p1.x - p2.x);
+				return p1.y < p2.y ? -1 : 1;
 			}
+
+			if  (p1.x != p2.x){
+				return p1.x < p2.x ? -1 : 1;
+			}
+
+			return 0;
 		}
 	}
 
