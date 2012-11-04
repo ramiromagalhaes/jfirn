3c34db987bde796aeb3eea6b9ed533798c67950a
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-04 19:04:12 -0200

    Triangle is now a inner class.

--- src/main/java/br/ufrj/jfirn/simulator/renderer/SimpleSwingRenderer.java
+++ src/main/java/br/ufrj/jfirn/simulator/renderer/SimpleSwingRenderer.java
@@ -16,6 +16,8 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import org.apache.commons.math3.util.FastMath;
+
 import br.ufrj.jfirn.common.Point;
 import br.ufrj.jfirn.common.Robot;
 
@@ -128,5 +130,24 @@ public class SimpleSwingRenderer implements SimulationRenderer, ChangeListener {
 		frame.repaint();
 	}
 
+	private static class Triangle {
+		public final int x[], y[], n = 3;
+
+		private final double alpha = FastMath.PI / 6d + FastMath.PI / 2d;
+		private final double beta = 10d;
+
+		public Triangle(final Point center, final double direction) {
+			x = new int[] {
+				(int)(center.x() + FastMath.cos(direction) * beta),
+				(int)(center.x() + FastMath.cos(direction + alpha) * beta / 2d),
+				(int)(center.x() + FastMath.cos(direction - alpha) * beta / 2d)
+			};
+			y = new int[] {
+				(int)(center.y() - FastMath.sin(direction) * beta),
+				(int)(center.y() - FastMath.sin(direction + alpha) * beta / 2d),
+				(int)(center.y() - FastMath.sin(direction - alpha) * beta / 2d)
+			};
+		}
+	}
 
 }
