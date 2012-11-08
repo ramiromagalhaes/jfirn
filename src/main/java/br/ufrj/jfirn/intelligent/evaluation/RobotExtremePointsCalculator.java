75e5b372ce1ea2fe8f55baf57ceab88b459bccdd
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2012-11-08 11:19:14 -0200

    Tentative documentation.

--- src/main/java/br/ufrj/jfirn/intelligent/evaluation/RobotExtremePointsCalculator.java
+++ src/main/java/br/ufrj/jfirn/intelligent/evaluation/RobotExtremePointsCalculator.java
@@ -3,10 +3,17 @@ package br.ufrj.jfirn.intelligent.evaluation;
 import org.apache.commons.math3.util.FastMath;
 
 import br.ufrj.jfirn.common.Point;
+import br.ufrj.jfirn.common.Robot;
 
 public class RobotExtremePointsCalculator {
 	private static final double RADIUS = 10;
 
+	/**
+	 * Get 2 points that are 10 units away from myPosition and located at
+	 * -PI/2 an PI/2 from myDirection.
+	 * 
+	 * This is useful to know the limits of a {@link Robot}.
+	 */
 	//TODO gimme a decent name!
 	public static Point[] makePoint(Point myPosition, double myDirection) {
 		final double d1 = myDirection - FastMath.PI / 2d;
