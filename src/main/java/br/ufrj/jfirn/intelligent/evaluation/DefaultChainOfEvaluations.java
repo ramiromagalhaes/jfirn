2529f6d36748aeca2d556627cf5c3a344acb201e
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-01-18 14:04:04 -0200

    Removing unused code.

--- src/main/java/br/ufrj/jfirn/intelligent/evaluation/DefaultChainOfEvaluations.java
+++ src/main/java/br/ufrj/jfirn/intelligent/evaluation/DefaultChainOfEvaluations.java
@@ -16,7 +16,6 @@ public class DefaultChainOfEvaluations implements ChainOfEvaluations, ThoughtPro
 		evaluators.add(new NextTargetEvaluator());
 		evaluators.add(new QuickCollisionEvaluator()); //You can try replacing this line with the one bellow
 		                                               //evaluators.add(new QuickCollisionWithoutCollisionTimeSimilaritiesEvaluator());
-        //evaluators.add(new QuickCollisionWithoutCollisionTimeSimilaritiesEvaluator());
 		evaluators.add(new CollisionProbabilityEvaluator());
 	}
 
