18fb31a04beb06cef90be6362f20da00b67c4c39
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-01-18 11:34:35 -0200

    Trajectory got more tests.

--- src/test/java/br/ufrj/jfirn/intelligent/TrajectoryTest.java
+++ src/test/java/br/ufrj/jfirn/intelligent/TrajectoryTest.java
@@ -6,30 +6,45 @@ import org.apache.commons.math3.util.FastMath;
 import org.junit.Test;
 
 import br.ufrj.jfirn.common.Point;
-import br.ufrj.jfirn.intelligent.Trajectory;
+import br.ufrj.jfirn.common.Robot;
 
 public class TrajectoryTest {
 	private static final double TEN_TO_MINUS_3 = .001;
 
 	@Test
-	public void testEval() {
+	public void testIntersection1() {
 		Trajectory t1 = new Trajectory(FastMath.PI / 4d, new Point(-10, -10));
 		Trajectory t2 = new Trajectory(FastMath.PI * 3d / 4d, new Point(10, -10));
 		Point intersection = t1.intersect(t2);
 		assertEquals(0d, intersection.x(), TEN_TO_MINUS_3);
 		assertEquals(0d, intersection.y(), TEN_TO_MINUS_3);
+	}
 
-		t1 = new Trajectory(1.1071, new Point(0, 3));
-		t2 = new Trajectory(-.4636, new Point(0, 7));
-		intersection = t1.intersect(t2);
+	@Test
+	public void testIntersection2() {
+		Trajectory t1 = new Trajectory(1.1071, new Point(0, 3));
+		Trajectory t2 = new Trajectory(-.4636, new Point(0, 7));
+		Point intersection = t1.intersect(t2);
 		assertEquals(1.6d, intersection.x(), TEN_TO_MINUS_3);
 		assertEquals(6.2d, intersection.y(), TEN_TO_MINUS_3);
+	}
 
-		t1 = new Trajectory(0, new Point(0, 4));
-		t2 = new Trajectory(FastMath.PI / 4d, new Point(0, 0));
-		intersection = t1.intersect(t2);
+	@Test
+	public void testIntersection3() {
+		Trajectory t1 = new Trajectory(0, new Point(0, 4));
+		Trajectory t2 = new Trajectory(FastMath.PI / 4d, new Point(0, 0));
+		Point intersection = t1.intersect(t2);
 		assertEquals(4d, intersection.x(), TEN_TO_MINUS_3);
 		assertEquals(4d, intersection.y(), TEN_TO_MINUS_3);
+	}
+
+	@Test
+	public void testIntersection() {
+		Trajectory t1 = new Trajectory(Robot.DOWN, new Point(300, 280));
+		Trajectory t2 = new Trajectory(new Point(200, 200), new Point(400, 100));
+		Point intersection = t1.intersect(t2);
+		assertEquals(300, intersection.x(), TEN_TO_MINUS_3);
+		assertEquals(256, intersection.y(), TEN_TO_MINUS_3);
 	}
 
 }
