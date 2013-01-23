afda89ff9bcd8734e8fe60da015a74f782059ed1
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-01-23 17:11:00 -0200

    A method.

--- src/main/java/br/ufrj/jfirn/intelligent/evaluation/RobotsConstants.java
+++ src/main/java/br/ufrj/jfirn/intelligent/evaluation/RobotsConstants.java
@@ -1,5 +1,16 @@
 package br.ufrj.jfirn.intelligent.evaluation;
 
+import br.ufrj.jfirn.common.Point;
+
 public class RobotsConstants {
+
 	public final static double SIZE_RADIUS = 5;
+
+	/**
+	 * Used to verify if the robot is in danger.
+	 */
+	public static boolean isInDangerRadius(Point myPosition, Point otherPosition) {
+		return myPosition.distanceTo(otherPosition) <= RobotsConstants.SIZE_RADIUS * 2.5d;
+	}
+
 }
